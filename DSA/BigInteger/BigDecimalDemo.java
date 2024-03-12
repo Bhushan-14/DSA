c9c7027237afc039e54e47f4246d9eac016b05c4
Bhushan-714 <bhushanpatil.rcpit@gmail.com>
2024-03-12 08:38:10 +0530

    commit successfully

--- DSA/BigInteger/BigDecimalDemo.java
+++ DSA/BigInteger/BigDecimalDemo.java
@@ -12,5 +12,13 @@ public class BigDecimalDemo {
         BigDecimal Y = new BigDecimal("0.04");
         BigDecimal ans = X.subtract(Y);
         System.out.println(ans);
+
+        BigDecimal A = new BigDecimal("32536547.252525254");
+        BigDecimal B = new BigDecimal("54234563625.324141241");
+        System.out.println(B.add(A));
+        System.out.println(B.subtract(B));
+        System.out.println(B.multiply(A));
+        System.out.println(B.pow(6));
+        System.out.println(B.scale());
     }
 }
