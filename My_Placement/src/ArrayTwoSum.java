d7bb3b234f6285be4e231a6168c183d8d8981a0f
Bhushan Patil <139554602+Bhushan-14@users.noreply.github.com>
2025-01-14 15:33:17 +0000

    scuccess new start

--- My_Placement/src/ArrayTwoSum.java
+++
@@ -1,5 +0,0 @@
-public class ArrayTwoSum {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ NewStart/ArrayOne.java
@@ -0,0 +1,15 @@
+/*
+    Array :
+    - A collection of data elements of the same type stored in contiguous memory locations.
+    - It is denoted by square brackets ([]).
+    - Elements are accessed using index.
+    - Index starts from 0.
+*/
+import java.util.Scanner;
+public class ArrayOne{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+    }
+}
