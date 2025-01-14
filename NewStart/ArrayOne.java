f5bf97839b08ecd649d6fa86098b51beb8525edd
Bhushan Patil <139554602+Bhushan-14@users.noreply.github.com>
2025-01-14 15:43:08 +0000

    success

--- NewStart/ArrayOne.java
+++ NewStart/ArrayOne.java
@@ -5,11 +5,21 @@
     - Elements are accessed using index.
     - Index starts from 0.
 */
+
+import java.util.Arrays;
 import java.util.Scanner;
 public class ArrayOne{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of elements in the array: ");
         int n = sc.nextInt();
+
+        int[] arr = new int[n]; // Declaration and initialization of an array with n elements
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Entered array is: "+Arrays.toString(arr));
+
     }
 }
